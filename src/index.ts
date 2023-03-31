df9d6e6080bd0e909092d5e1065cade79fa48599
Mirania <blakbgtr@gmail.com>
2023-03-31 01:17:50 +0100

    set status on init

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ Promise.all([
 
 client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
+    client.user.setPresence({ activities: [{ name: 'Birthday Bot! /register' }], status: 'dnd' });
     announcer.execute(client);
     setInterval(() => announcer.execute(client), announcer.cooldownMs);
 });
